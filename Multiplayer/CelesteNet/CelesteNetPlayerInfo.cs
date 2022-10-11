789f9508b9ea1791c6d1c90b95c47f8c6d1f6563
Brokemia <37253216+Brokemia@users.noreply.github.com>
2022-10-11 15:20:17 -0400

    Add more data to multiplayer PlayerInfo

--- Multiplayer/CelesteNet/CelesteNetPlayerInfo.cs
+++ Multiplayer/CelesteNet/CelesteNetPlayerInfo.cs
@@ -1,5 +1,6 @@
 ﻿using Celeste.Mod.CelesteNet.Client;
 using Celeste.Mod.CelesteNet.DataTypes;
+using Microsoft.Xna.Framework;
 
 namespace MadelineParty.Multiplayer.CelesteNet {
     public class CelesteNetPlayerInfo : PlayerInfo {
@@ -13,5 +14,11 @@ namespace MadelineParty.Multiplayer.CelesteNet {
         public override uint ID => id;
 
         public override string Name => name ?? (CelesteNetClientModule.Instance.Client.Data.TryGetRef(id, out DataPlayerInfo value) ? name = value.Name : "???");
+        
+        public override string Level => (CelesteNetClientModule.Instance.Client.Data.TryGetRef(id, out DataPlayerState value) ? value.Level : null);
+        
+        public override string SID => (CelesteNetClientModule.Instance.Client.Data.TryGetRef(id, out DataPlayerState value) ? value.SID : null);
+        
+        public override Vector2 Position => (CelesteNetClientModule.Instance.Client.Data.TryGetRef(id, out DataPlayerFrame value) ? value.Position : default);
     }
 }
--- Multiplayer/PlayerInfo.cs
+++ Multiplayer/PlayerInfo.cs
@@ -1,8 +1,16 @@
-﻿namespace MadelineParty.Multiplayer {
+﻿using Microsoft.Xna.Framework;
+
+namespace MadelineParty.Multiplayer {
     public abstract class PlayerInfo {
         public abstract uint ID { get; }
 
         public abstract string Name { get; }
+
+        public abstract string Level { get; }
+        
+        public abstract string SID { get; }
+        
+        public abstract Vector2 Position { get; }
     }
 
     public class DummyPlayerInfo : PlayerInfo {
@@ -12,6 +20,18 @@
 
         public string _Name = "???";
 
+        public string _Level = null;
+
+        public string _SID = null;
+
+        public Vector2 _Position;
+
         public override string Name => _Name;
+
+        public override string Level => _Level;
+
+        public override string SID => _SID;
+
+        public override Vector2 Position => _Position;
     }
 }
